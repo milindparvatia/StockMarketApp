8d9b821275c1d032f92c03a8bc13a5e85c233abc
Milind Parvatia <milind1997@gmail.com>
2018-09-22 15:08:05 +0530

    sma

--- app/views.py
+++ app/views.py
@@ -15,6 +15,14 @@ from .forms import NameForm
 from twitterscraper import query_tweets
 import os
 from urllib.parse import unquote
+import json
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
+from collections import defaultdict
+import re
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyser = SentimentIntensityAnalyzer()
 
 def get_name(request):
     # if this is a POST request we need to process the form data
@@ -26,6 +34,7 @@ def get_name(request):
         Array = valArray.split(":")
         value = Array[1]
         val = valArray
+
         with open('searchVal.txt','w+') as f:
             #convert to string:
             f.seek(0)
@@ -33,11 +42,10 @@ def get_name(request):
             f.truncate()
             f.close()
         # redirect to a new URL:
-        url = ('https://newsapi.org/v2/everything?q="'+value+'"&apiKey=4df8d4c46e5f41bca7e6e1331b63ad7d')
-        response = requests.get(url)
+        response = requests.get('https://newsapi.org/v2/everything?q="'+value+'"&apiKey=4df8d4c46e5f41bca7e6e1331b63ad7d')
         geodata = response.json()
             
-        return render(request, 'app/search.html', {'allnews': geodata['articles'],'val': val})
+        return render(request, 'app/search.html', {'allnews': geodata['articles'],'val': val,'value':value})
     else:
         form = NameForm()
         return render(request, 'app/name.html', {'form': form})
@@ -82,7 +90,7 @@ class TimeSeriesDailyAdjusted(APIView):
         # dataval = unquote(dataval)
         search_val = open('searchVal.txt','r').read()
         
-        data=requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='+search_val+'&outputsize=compact&apikey=6G6EDTRGV2N1F9SP')
+        data=requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='+search_val+'&outputsize=full&apikey=6G6EDTRGV2N1F9SP')
         data=data.json()
         data=data['Time Series (Daily)']
         df=pd.DataFrame(columns=['date','open','high','low','close','volume'])
@@ -94,48 +102,77 @@ class TimeSeriesDailyAdjusted(APIView):
                 df.index=df.index+1
         dataDaily=df.sort_values('date')
 
-        data=requests.get('https://www.alphavantage.co/query?function=MACD&symbol='+search_val+'&interval=daily&series_type=open&apikey=6G6EDTRGV2N1F9SP')
+        data=requests.get('https://www.alphavantage.co/query?function=EMA&symbol='+search_val+'&interval=weekly&time_period=9&series_type=open&apikey=6G6EDTRGV2N1F9SP')
         data=data.json()
-        data=data['Technical Analysis: MACD']
-        df=pd.DataFrame(columns=['date','MACD_Signal','MACD','MACD_Hist'])
+        data=data['Technical Analysis: EMA']
+        df=pd.DataFrame(columns=['date','EMA'])
         for d,p in data.items():
             date=datetime.datetime.strptime(d,'%Y-%m-%d')
-            data_row=[date,float(p['MACD_Signal']),float(p['MACD']),float(p['MACD_Hist'])]
+            data_row=[date,float(p['EMA'])]
             df.loc[-1,:]=data_row
             df.index=df.index+1
-        dataMACD=df.sort_values('date')
+        dataEMA1=df.sort_values('date')
 
+
+        data=requests.get('https://www.alphavantage.co/query?function=EMA&symbol='+search_val+'&interval=weekly&time_period=26&series_type=open&apikey=6G6EDTRGV2N1F9SP')
+        data=data.json()
+        data=data['Technical Analysis: EMA']
+        df=pd.DataFrame(columns=['date','EMA'])
+        for d,p in data.items():
+            date=datetime.datetime.strptime(d,'%Y-%m-%d')
+            data_row=[date,float(p['EMA'])]
+            df.loc[-1,:]=data_row
+            df.index=df.index+1
+        dataEMA2=df.sort_values('date')
+
+
+        os.remove("tweets1234.json")
+        Array = search_val.split(":")
+        value = Array[1]
+        os.system('twitterscraper #'+value+' --limit 50 -bd 2018-09-17 -ed 2018-09-20 --output=tweets1234.json')
+        punctuation = list(string.punctuation)
+        stop = stopwords.words('english') + punctuation + ['rt', 'via']
+        
+        with open('tweets1234.json', 'r') as f:
+            line = f.read() # read only the first tweet/line
+            total = list()
+            sentiment = 0.0
+            pos = 0.0
+            neg = 0.0
+            tweet = json.loads(line) # load it as Python dict
+            type(tweet)
+            for key in tweet:
+                snt = analyser.polarity_scores(key['text'])
+                sentiment = sentiment + snt['compound']
+                pos = pos + snt['pos']
+                neg = neg + snt['neg']
+                terms_stop = [term for term in word_tokenize(key['text']) if term not in stop] #Using Nltk to tokenize
+                total.extend(terms_stop)
+        
+        for key in total:
+            if(len(key) < 3):
+                total.remove(key)
+
+        for i in range(len(total)):
+            total[i] = total[i].lower()
+
+        
+        f.close()
+
+        sentiment=str(sentiment)
+        str(neg)
+        str(pos)
+
+        defaultEMA1 = dataEMA1
+        defaultEMA2 = dataEMA2
         defaultDaily = dataDaily
-        defaultMACD = dataMACD
         alldata = {
             "defaultDaily": defaultDaily,
-            "defaultMACD": defaultMACD,
+            "sentiment": sentiment,
+            "defaultEMA2":defaultEMA2,
+            "defaultEMA1":defaultEMA1,
         }
         return Response(alldata)
-
-
-# class MACD(APIView):
-#     authentication_classes = []
-#     permission_classes = []
-        
-#     def get(self, request, format=None):
-#         search_val = open('searchVal.txt','r').read()
-#         data=requests.get('https://www.alphavantage.co/query?function=MACD&symbol='+search_val+'&interval=daily&series_type=open&apikey=6G6EDTRGV2N1F9SP')
-#         data=data.json()
-#         data=data['Technical Analysis: MACD']
-#         df=pd.DataFrame(columns=['date','MACD_Signal','MACD','MACD_Hist'])
-#         for d,p in data.items():
-#             date=datetime.datetime.strptime(d,'%Y-%m-%d')
-#             data_row=[date,float(p['MACD_Signal']),float(p['MACD']),float(p['MACD_Hist'])]
-#             df.loc[-1,:]=data_row
-#             df.index=df.index+1
-#         data=df.sort_values('date')
-
-#         default_items = data
-#         alldata = {
-#                 "default": default_items,
-#         }
-#         return Response(alldata)
 
 class Tweeter(APIView):
     authentication_classes = []
